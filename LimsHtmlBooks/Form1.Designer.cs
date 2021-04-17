8c2f786928543f0bb4d148056b169465659cbe8a
马洪彪 <e_yuan@foxmail.com>
2021-04-17 16:21:31 +0800

    1

---
+++ LimsHtmlBooks/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LimsHtmlBooks
+{
+    partial class FmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExport = new System.Windows.Forms.Button();
+            this.bgw = new System.ComponentModel.BackgroundWorker();
+            this.pb = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboBookName = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(494, 20);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(100, 29);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "导出";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // bgw
+            // 
+            this.bgw.WorkerReportsProgress = true;
+            this.bgw.WorkerSupportsCancellation = true;
+            this.bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_DoWork);
+            this.bgw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw_ProgressChanged);
+            this.bgw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw_RunWorkerCompleted);
+            // 
+            // pb
+            // 
+            this.pb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb.Location = new System.Drawing.Point(13, 84);
+            this.pb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(832, 29);
+            this.pb.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "手册类型：";
+            // 
+            // cboBookName
+            // 
+            this.cboBookName.FormattingEnabled = true;
+            this.cboBookName.Location = new System.Drawing.Point(133, 20);
+            this.cboBookName.Name = "cboBookName";
+            this.cboBookName.Size = new System.Drawing.Size(288, 23);
+            this.cboBookName.TabIndex = 3;
+            // 
+            // FmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 151);
+            this.Controls.Add(this.cboBookName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.btnExport);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FmMain";
+            this.Text = "starlims手册导出工具";
+            this.Load += new System.EventHandler(this.FmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExport;
+        private System.ComponentModel.BackgroundWorker bgw;
+        private System.Windows.Forms.ProgressBar pb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboBookName;
+    }
+}
+
---
+++ LimsHtmlBooks/Form1.cs
@@ -0,0 +1,85 @@
+﻿using LimsHtmlBooks.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LimsHtmlBooks
+{
+    public partial class FmMain : Form
+    {
+        string bookname = "";
+        public FmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            bookname = cboBookName.Text;
+            string sql = "SELECT COUNT(1) FROM LIMSHTMLBOOKS WHERE BOOKNAME = '"+ bookname + "' ";
+            int count = (int)SQLHelper.ExecuteScalar(sql);
+            pb.Maximum = count;
+            bgw.RunWorkerAsync();
+        }
+
+        private void bgw_DoWork(object sender, DoWorkEventArgs e)
+        { 
+            string sql = "SELECT FILEPATH, FILECONTENT FROM LIMSHTMLBOOKS WHERE BOOKNAME = '"+ bookname + "' ";
+            DataTable dataTable = SQLHelper.GetDataTable(sql);
+            int progressValue = 0;
+            foreach(DataRow dataRow in dataTable.Rows)
+            {
+                string FILEPATH = dataRow["FILEPATH"].ToString();//文件目录
+                FILEPATH = bookname + '/' + FILEPATH;
+                byte[] FILECONTENT = (byte[])dataRow["FILECONTENT"];//文件内容
+                if (FILEPATH.IndexOf('/') != -1)//子目录文件，则需要创建目录（文件夹）
+                {
+                    FileInfo fileInfo = new FileInfo(FILEPATH);
+                    DirectoryInfo directoryInfo = fileInfo.Directory;
+                    if (!directoryInfo.Exists)
+                    {
+                        directoryInfo.Create();
+                    }
+                }
+                //写入文件
+                if (FILECONTENT.Length > 0)
+                {
+                    FileStream fs = File.Create(FILEPATH, FILECONTENT.Length);
+                    fs.Write(FILECONTENT, 0, FILECONTENT.Length);
+                    fs.Close();
+                } 
+                // 
+                bgw.ReportProgress(++progressValue);
+            }
+
+        }
+
+        private void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pb.Value = e.ProgressPercentage;
+        }
+
+        private void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("导出完成");
+        }
+
+        private void FmMain_Load(object sender, EventArgs e)
+        {
+            string sql = "SELECT distinct BOOKNAME FROM LIMSHTMLBOOKS  ";
+            DataTable dataTable = SQLHelper.GetDataTable(sql);
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                string bookname = dataRow[0].ToString();
+                cboBookName.Items.Add(bookname);
+            }
+        }
+    }
+}
